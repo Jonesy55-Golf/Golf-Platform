f3ee330d160d61a8cbb55aee78a3e2b0823399bd
Gary Jones <garyjones55@hotmail.com>
2026-02-13 15:19:00 -0800

    Events tab

---
+++ app/events/page.tsx
@@ -0,0 +1,8 @@
+export default function PlayersPage() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Players</h1>
+      <p>This is where the player list and player management tools will go.</p>
+    </main>
+  );
+}
