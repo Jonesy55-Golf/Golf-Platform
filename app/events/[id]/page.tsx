299d230d2f7d0f39033c38347948757f7dc2e06f
Gary Jones <garyjones55@hotmail.com>
2026-02-14 06:32:13 -0800

    Add Event button

---
+++ app/events/[id]/page.tsx
@@ -0,0 +1,12 @@
+<a href="/events/new">
+  <button
+    style={{
+      marginTop: "10px",
+      marginBottom: "20px",
+      padding: "10px 15px",
+      cursor: "pointer",
+    }}
+  >
+    Add Event
+  </button>
+</a>
